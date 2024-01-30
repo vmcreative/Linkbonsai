6728a67c61651c11061a2e833a3869db837081d4
Vincent Martin <vincent@brooklinen.com>
2024-01-30 10:23:48 -0500

    debugging

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+console.log('server.js is running...');
+console.log(`Current directory: ${__dirname}`);
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
